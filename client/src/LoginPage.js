1761dffb10e7678eb8a5b506637390534620861e
Lucas Sweeney <lsweeney0124@gmail.com>
2022-07-29 08:18:39 -0600

    added change and submit functions

--- client/src/LoginPage.js
+++ client/src/LoginPage.js
@@ -1,4 +1,27 @@
+import { useState } from "react"
+// import { useNavigate } from "react-router-dom"
+
 function LoginPage () {
+  const [ formData, setFormData ] = useState('')
+  // const [ isLogged, setIsLogged ] = useState(false)
+  // const history = useNavigate()
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [ e.target.name ]: e.target.value
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // add fetch here, POST request, URL is proxied already so just use route from backend.
+    alert('User has logged in: ' + formData.name)
+    console.log(formData);
+  }
+
+
+
   return (
     <div>
       <h1 className="title">
@@ -10,12 +33,13 @@ function LoginPage () {
             Welcome to CSS, where you can buy or sell items at your local garage sale!
           </div>
         </div>
-        <div id="login_form">
+        <form id="login_form" onSubmit={ handleSubmit }>
           <label>Username:</label><br />
-          <input type="text" name="name" /><br />
+          <input type="text" name="name" onChange={ handleChange } value={ formData.name } /><br />
           <label>Password:</label>
-          <input type="password" name="pass" />
-        </div>
+          <input type="password" name="pass" onChange={ handleChange } value={ formData.pass } />
+          <button id="submit_login" type="submit">Submit Login</button>
+        </form>
       </div>
     </div>
   )
