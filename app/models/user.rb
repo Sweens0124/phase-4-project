08878a9ddaa6b87177c6e3ba36da408c46777e3f
Lucas Sweeney <lsweeney0124@gmail.com>
2022-07-29 09:16:00 -0600

    grammar fixed ;)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-    has_mamy :items
+    has_many :items
     has_many :garage_sales, through: :items
 
-    validates :name, presence: true, uniquness: true
-    validates :email, presence: true, uniquness: true
+    validates :name, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
 
 end
