edecdd4d3d305a77a646f69b15081c6bfd7e296e
Lucas Sweeney <lsweeney0124@gmail.com>
2022-08-01 17:00:58 -0600

    i dont even know

--- client/src/ItemForm.js
+++ client/src/ItemForm.js
@@ -4,134 +4,112 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 
-function ItemForm({onAddItem}) {
-    const [show, setShow] = useState(false);
-    const [enteredName, setName] = useState('');
-    const [enteredDescription, setDescription] = useState('');
-    const [enteredCategory, setCategory] = useState(0);
-    const [enteredCondition, setCondition] = useState(0);
-    const [enteredPrice, setPrice] = useState('');
-    const [enteredImageUrl, setImageUrl] = useState('');
+function ItemForm ({ addItem }) {
+    const [ show, setShow ] = useState(true);
+    const [ formData, setFormData ] = useState({
+        name: '',
+        description: '',
+        category: '',
+        condition: '',
+        price: '',
+    })
+    const [ errors, setErrors ] = useState([])
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData({ ...formData, [ name ]: value })
+    }
+
+    function onSubmit (e) {
+        e.preventDefault()
+
+        fetch(`/items`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ ...formData, ongoing: true })
+        })
+            .then(res => {
+                if (res.ok) {
+                    res.json().then(addItem)
+                } else {
+                    //Display errors
+                    res.json().then(data => {
+                        // debugger
+                        setErrors(Object.entries(data.errors))
+                    })
+                }
+            })
+    }
 
 
     const handleClose = () => {
-        setName('name')
-        setDescription('')
-        setCategory(0)
-        setImageUrl('imageUrl')
-        setCondition(0)
-        setPrice('')
         setShow(false)
     };
-    const handleShow = () => setShow(true);
 
-    const nameChangeHandler = (event) => {
-        setName(event.target.value);
-    }
 
-    const descriptionChangeHandler = (event) => {
-        setDescription(event.target.value);
-    }
-
-    const imageUrlChangeHandler = (event) => {
-        setImageUrl(event.target.value);
-    }
-
-    const categoryChangeHandler = (event) => {
-        console.log(event);
-        setCategory(event.target.value);
-    }
-
-    const priceChangeHandler = (event) => {
-        console.log(event);
-        setPrice(event.target.value);
-    }
-
-    const conditionChangeHandler = (event) => {
-        setCondition(event.target.value);
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const newItem = {
-            "name": enteredName,
-            "description": enteredDescription,
-            "category": enteredCategory,
-            "price": enteredPrice,
-            "imageUrl": enteredImageUrl,
-            "condition": enteredCondition
-        }
-        onAddItem(newItem);
-        handleClose();
-    }
 
     return (
         <>
-        <Button id="newItem" variant="dark" onClick={handleShow}>
-            Add Item
-        </Button>
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>Add New Item</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group className="mb-3" controlId="formBasicName">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control value={enteredName} onChange={nameChangeHandler} placeholder="Enter Name" />
-                    </Form.Group>
+            <Modal show={ show } onHide={ handleClose }>
+                <Modal.Title>Add New Item:</Modal.Title>
+                <Modal.Body>
+                    <Form onSubmit={ onSubmit }>
+                        <Form.Group className="mb-3" controlId="formBasicName">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control name='name' value={ formData.name } onChange={ handleChange } placeholder="Enter Name" />
+                        </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPrice">
-                        <Form.Label>Price</Form.Label>
-                        <Form.Control value={enteredPrice} onChange={priceChangeHandler} placeholder="Price" />
-                    </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPrice">
+                            <Form.Label>Price</Form.Label>
+                            <Form.Control name='price' value={ formData.price } onChange={ handleChange } placeholder="Price" />
+                        </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicDescription">
-                        <Form.Label>Description</Form.Label>
-                        <Form.Control value={enteredDescription} onChange={descriptionChangeHandler} placeholder="Description" />
-                    </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicDescription">
+                            <Form.Label>Description</Form.Label>
+                            <Form.Control name='description' value={ formData.description } onChange={ handleChange } placeholder="Description" />
+                        </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicSelect">
-                        <Form.Label>Category</Form.Label>
-                        <Form.Control value={enteredCategory} hidden />
-                        <Form.Select value={enteredCategory} onChange={categoryChangeHandler}>
-                            <option value="0">Select a Category</option>
-                            <option value="1">Music</option>
-                            <option value="2">Electronics</option>
-                            <option value="3">Furniture</option>
-                            <option value="4">Sports</option>
-                            <option value="5">Clothing</option>
-                            <option value="6">Appliances</option>
-                        </Form.Select>
-                    </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicSelect">
+                            <Form.Label>Category</Form.Label>
+                            {/* <Form.Control value={ formData.category } hidden /> */ }
+                            <Form.Select value={ formData.category } onChange={ handleChange }>
+                                <option value="0">Select a Category</option>
+                                <option value="1">Music</option>
+                                <option value="2">Electronics</option>
+                                <option value="3">Furniture</option>
+                                <option value="4">Sports</option>
+                                <option value="5">Clothing</option>
+                                <option value="6">Appliances</option>
+                            </Form.Select>
+                        </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicSelect">
-                        <Form.Label>Condition</Form.Label>
-                        <Form.Control value={enteredCondition} hidden />
-                        <Form.Select value={enteredCondition} onChange={conditionChangeHandler}>
-                            <option value="0">Select a Condition</option>
-                            <option value="1">Great</option>
-                            <option value="2">Good</option>
-                            <option value="3">Bad</option>
-                        </Form.Select>
-                    </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicSelect">
+                            <Form.Label>Condition</Form.Label>
+                            <Form.Control value={ formData.condition } hidden />
+                            <Form.Select value={ formData.condition } onChange={ handleChange }>
+                                <option value="0">Select a Condition</option>
+                                <option value="Great">Great</option>
+                                <option value="Good">Good</option>
+                                <option value="Bad">Bad</option>
+                            </Form.Select>
+                        </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicImageUrl">
-                        <Form.Label>Item Image Url</Form.Label>
-                        <Form.Control value={enteredImageUrl} onChange={imageUrlChangeHandler} placeholder=""/>
-                    </Form.Group>
+                        {/* <Form.Group className="mb-3" controlId="formBasicImageUrl">
+                            <Form.Label>Item Image Url</Form.Label>
+                            <Form.Control value={ enteredImageUrl } onChange={ imageUrlChangeHandler } placeholder="" />
+                        </Form.Group> */}
 
-                    <ButtonGroup className="me-2" aria-label="Submit Button">
-                        <Button variant="outline-primary" type="submit">
-                            Submit
-                        </Button>
-                    </ButtonGroup>
-                </Form>
-            </Modal.Body>
-        </Modal>
+                        <ButtonGroup className="me-2" aria-label="Submit Button">
+                            <Button variant="outline-primary" type="submit">
+                                Submit
+                            </Button>
+                        </ButtonGroup>
+                    </Form>
+                </Modal.Body>
+            </Modal>
         </>
     );
-    }
+}
 
 
 export default ItemForm;
