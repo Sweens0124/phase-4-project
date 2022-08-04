c642d3b632fe2977ce1b6ac06fdc701f4ef06a18
Lucas Sweeney <lsweeney0124@gmail.com>
2022-08-04 09:53:55 -0600

    minor changes to errors happening in console

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -5,7 +5,6 @@ import { NavLink, useNavigate } from "react-router-dom";
 import BrowseItems from './BrowseItems';
 
 const Navbar = ({ loggedInUserId, isLoggedIn, setIsLoggedIn }) => {
-    console.log(loggedInUserId)
     const history = useNavigate();
 
     function handleLogout (e) {
@@ -23,12 +22,12 @@ const Navbar = ({ loggedInUserId, isLoggedIn, setIsLoggedIn }) => {
                 <NavLink className="navlink" to="/browse-items" element={ <BrowseItems /> }> Browse Items </NavLink>
                 <NavLink className="navlink" to="/contact"> Contact </NavLink>
                 <div className='avatar'>
-                <NavLink className="navlink" to={`/users/${loggedInUserId}`} element={ <BrowseItems /> }>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-                    </svg>
-                </NavLink>
+                    <NavLink className="navlink" to={ `/users/${loggedInUserId}` } element={ <BrowseItems /> }>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                            <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                        </svg>
+                    </NavLink>
                 </div>
                 <Form onSubmit={ handleLogout }>
                     { renderLogout }
